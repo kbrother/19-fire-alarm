b42a6560e35351d5ccc18e234c13cf9722df2207
kbrother <00076289@kaist.ac.kr>
2021-07-04 21:02:18 +0900

    minor changes

--- main.py
+++ main.py
@@ -15,17 +15,23 @@ def on_connect(client, userdata, flags, rc):
 def raw_data_callback(client, userdata, msg):
     print(msg.payload)
     curr_dict = json.loads(msg.payload)
-    for i in range(len(curr_dict["cfd"])):
-        if curr_idx < window_len:
-            data_stream_list[i].build_window(curr_dict)
-            if curr_idx == window_len-1:
+    if curr_idx % 3 == 0:
+        for i in range(len(curr_dict["cfd"]) - 1):
+            if curr_idx < window_len:
+                data_stream_list[i].build_window(curr_dict)
+                if curr_idx == window_len-1:
+                    normed_mat = data_stream_list[i].normalize_matrix(data_stream_list[i].curr_mat)
+                    mat_model_list[i].GD_initialize(normed_mat, 100)
+            else:
+                data_stream_list[i].update_window(curr_dict)
                 normed_mat = data_stream_list[i].normalize_matrix(data_stream_list[i].curr_mat)
-                mat_model_list[i].GD_initialize(normed_mat, 100)
-        else:
-            data_stream_list[i].update_window(curr_dict)
-            normed_mat = data_stream_list[i].normalize_matrix(data_stream_list[i].curr_mat)
-            mat_model_list[i].run(normed_mat)
-            X_tilde = data_stream_list[i].denormalize_matrix(np.matmul(mat_model_list[i].P, mat_model_list[i].Q.transpose()))
+                mat_model_list[i].run(normed_mat)
+                X_tilde = data_stream_list[i].denormalize_matrix(np.matmul(mat_model_list[i].P, mat_model_list[i].Q.transpose()))
+
+                print_str = f'{curr_time}, '
+                for i in range(mat_model_list[i].num_sensor):
+                    print_str = print_str + f'{data_stream_list[i].curr_mat[print_node, i]}, {X_tilde[print_node, i]},'
+                print_str = print_str + "\n"
 
     curr_idx += 1
     # matrix_list = json_parser(msg.payload)
@@ -55,7 +61,7 @@ if __name__ == "__main__":
     parser.add_argument('--num-channel', type=int, default=1, help="the number of channel")
     args = parser.parse_args()
 
-    curr_idx = 0
+    curr_idx, print_node = 0, 0
     mat_model_list = [model(args.num_node, args.num_sensor, 5, 1e7, 1e-4) for _ in range(args.num_channel)]
     data_stream_list = [data(args.num_node, args.num_sensor, args.window_len) for _ in range(args.window_len)]
     main(args.host, args.port)
